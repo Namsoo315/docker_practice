558a6fffdc49b97fc7b833d7ec9b952d1a83ae44
Namsoo315 <gustn2130@naver.com>
2025-09-26 09:39:46 +0900

    feat: 이미지 삭제, 조회, 등록 추가
    이미지 삭제를 SH에 추가
    이미지 조회를 SH에 추가

--- docker_practice/01_docker/01_basic/01_docker_basic.sh
+++ docker_practice/01_docker/01_basic/01_docker_basic.sh
@@ -21,4 +21,37 @@ docker pull amazoncorretto
 docker pull postgres:17.6
 docker pull amazoncorretto:17
 
-sha256:7b2a0022a2ad5814577d1c88ac703475dd9465b81871c12d97a0b796c90532c3
+docker pull amazoncorretto@sha256:7b2a0022a2ad5814577d1c88ac703475dd9465b81871c12d97a0b796c90532c3
+
+# 2) docker images
+# 현재 로컬 환경에 다운로드 되어 있는 Docker 이미지 목록을 조회하는 명령어이다.
+
+# 모든 이미지 보기
+docker images
+docker image ls
+
+# 특정 이미지만 보기
+docker images postgres
+docker images amazoncorretto
+
+# 사용하지 않는 이미지만 보기 (dangling)
+docker images --filter "dangling=true"
+
+# 이미지 ID 만 표시
+docker images -q
+
+# 3) docker rmi
+# 불필요한 이미지를 삭제하여 디스크 공간을 확보할 수 있다.
+
+# 이미지 ID로 삭제
+docker rmi [Image ID]
+
+# 이미지 이름:태그로 삭제
+docker rmi amazoncorretto:17
+
+# 사용하지 않는 이미지 모두 삭제
+docker image prune
+
+# 강제 삭제 (컨테이너가 사용하고 있어도 삭제 가능!)
+docker rmi -f amazoncorretto:17
+docker rmi -f [image Id, Image Name:Tag]
